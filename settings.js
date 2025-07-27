3319234c2a4a95c6e1e644fa570e1698343389ba
SJoker2006 <lahiruimesh847@gmail.com>
2025-07-27 10:58:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ global.botscript = process.env.BOTSCRIPT || "https://github.com/KaviDeveloperSe/
 global.packname = process.env.PACKNAME || "KAVI-X MD"
 global.author = process.env.AUTHOR || "Cyber Kavi"
 global.creator = process.env.CREATOR || "94702128378@s.whatsapp.net"
-global.botprefix = process.env.BOTPREFIX || "/"
+global.botprefix = process.env.BOTPREFIX || "."
 global.restart = process.env.RESTART || true
 global.mongoDB = process.env.MONGODB_URI || "mongodb+srv://ketkshqAqjXygEF5:ketkshqAqjXygEF5@freeuri.dmkyoml.mongodb.net/helloworld123456789?retryWrites=true&w=majority&appName=freeuri"
 let file = require.resolve(__filename)
